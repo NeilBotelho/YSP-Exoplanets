d7726b5c86f7280a9a454d31844922b67e4e6615
Neil Botelho <neil.botelho321@gmail.com>
2019-11-13 19:25:35 +0530

    Add Impute.R, ImputedNumericalCols.csv. Removed or renamed files to match

--- MostImportantFeatures/scripts/Impute.R
+++ MostImportantFeatures/scripts/Impute.R
@@ -1,7 +1,20 @@
 library(mice)
 library(dplyr)
-data<-read.csv("../numericalCols.csv")
-imputed = mice(data,method='rf',maxit=5)
-summary(imputed)
-completedData<-complete(imputed,1)
+
+#Read in Nasa exoplanets data
+df<-read.csv("../exoplanets.csv")
+
+#Define a function that returns 1 if more than 30% data is missing
+f<- function(x) { 
+    return(sum(is.na(x)) < length(x) * 0.4)
+}
+
+#Remove any column with more than 30% missing data
+df<-df[, vapply(df, f, logical(1)), drop = F]
+
+#Get only numeric columns
+numericCols<-dplyr::select_if(df, is.numeric)
+
+imputedCols = mice(numericCols,method='cart',maxit = 10,seed=40)
+completedData<-complete(imputedCols,1)
 write.csv(completedData,'../ImputedNumericCols.csv')
