c3aed26fd8b7e60712bd864bd422019eb6e5d450
Neil Botelho <neil.botelho321@gmail.com>
2019-11-16 17:41:06 +0530

    Add catboostModel scripts and data
    Add scopring script, useForRandom data and
    output(intermediateResults.csv)

--- MostImportantFeatures/scripts/Scoring.py
+++ MostImportantFeatures/scripts/Scoring.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-# from imblearn import SMOTE
-from xgboost import XGBClassifier
+from sklearn.linear_model import Perceptron
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -10,8 +9,20 @@ import random as r
 import sys
 r.seed(42)
 smote = SMOTE(ratio='minority')
-exos=pd.read_csv("../useForFitting.csv")
+exos=pd.read_csv("useForRandom.csv")
+print("Read exos")
 habitableRows=[2902, 3132, 2155, 2021, 1147, 2541, 2503, 1845, 2135, 2538, 3233, 2880, 2156, 163, 2097, 1424, 3115, 2883, 986, 2316, 1227, 2441, 2031, 2014, 3606, 2129, 703, 114, 2547, 1137, 128, 2189,151, 152, 153, 1604, 2155, 2223, 2882, 3133, 3606, 3716, 3742,3743, 3744]
+exosColumns=exos.columns
+
+
+penalty=[None, 'l2' , 'elasticnet']
+alpha=[ 0.01, 0.001,0.0001]
+max_iter=[100,200]
+tol=[0.00001,0.0000001,0.001]
+n_iter_no_change=[13,20]
+eta0=[1,10,20,70]
+
+
 def Preprocessing(df):
 	noncat=[x for x in df._get_numeric_data().columns]
 	cat=[x for x in df.columns if x not in noncat]
@@ -20,17 +31,18 @@ def Preprocessing(df):
 	validate=[]
 	Hcopy=[x for x in habitableRows if x in df.rowid]
 	if(len(Hcopy)<13):
+		# input("less")
 		return 0,0,0,0
 	numHidden=int(len(Hcopy)/2)
-	for i in range(numHidden): 
-	    randNum=r.randint(0,len(Hcopy)-1)
-	    validate.append(Hcopy[randNum])
-	    del Hcopy[randNum]
-	
+	for i in range(numHidden):
+		randNum=r.randint(0,len(Hcopy)-1)
+		validate.append(Hcopy[randNum])
+		del Hcopy[randNum]
+
 	while len(validate)<100:
-	    temp=r.randint(0,3500)
-	    if temp not in habitableRows:
-	        validate.append(temp)
+		temp=r.randint(0,3500)
+		if temp not in habitableRows:
+			validate.append(temp)
 	validate=df[df.rowid.isin(validate)]
 	d=df[~df.rowid.isin(validate.rowid)]
 	X=d[trainCols]
@@ -39,7 +51,8 @@ def Preprocessing(df):
 		X, y = smote.fit_sample(X, y)
 		validateX,validateY=smote.fit_sample(validate[trainCols],validate.habitable)
 	except ValueError:
-		return 0,0,0,0		
+		# input("Smte")
+		return 0,0,0,0
 	return X,y,validateX,validateY
 
 def score(Features):
@@ -47,39 +60,59 @@ def score(Features):
 	feats.append('habitable')
 	feats.append('rowid')
 	data=[]
-	data=exos[feats]
+	try:
+		data=exos[feats]
+	except KeyError:
+		feats=[x for x in feats if x in exosColumns]
+		data=exos[feats]
 	data=data.dropna()
 	if(len(data)<700):
+		# print(len(data))
+		# input()
 		return np.nan
 	X,y,validateX,validateY=Preprocessing(data)
 	if (type(X)==type(0)):
+		# input("Preprocessing")
 		return np.nan
 	trainX,testX,trainY,testY=train_test_split(X,y)
-	max_depth=[5,7]
-	learning_rate=[1,0.1,0.001]
-	n_estimators=[10,50,100,150]
-	booster=["gbtree","dart"]
+
 	best=-1
-	for l in learning_rate:
-	  for N in n_estimators:
-	    for b in [0,1]:
-	      for m in max_depth:
-	          testXGB=XGBClassifier(verbosity=0,max_depth=m,learning_rate=l,booster=booster[int(b)],n_estimators=N)
-	          testXGB.fit(trainX,trainY,eval_set=[(testX,testY)],early_stopping_rounds=10,verbose=False)
-	          b=balanced_accuracy_score(validateY,testXGB.predict(validateX))
-	          if best<b:
-	            best=b
+
+	for p in penalty:
+		for a in alpha:
+			for m in max_iter:
+				for t in tol:
+					for e in eta0:
+						for n in n_iter_no_change:
+							model= Perceptron(penalty=p,alpha=a,tol=t,n_iter_no_change=n,max_iter=m,n_jobs=-1,early_stopping=True,eta0=e)
+							model.fit(trainX,trainY)
+							y_preds=model.predict(validateX)
+							currScore=balanced_accuracy_score(validateY,y_preds)
+							if(currScore>best):
+								best=currScore
 	return best
 
-randomFeatureGroupings=pd.read_csv("../randomFeatureGroupings.csv")
-numChecked=0
-out=np.empty((0))
-for record in randomFeatureGroupings.features:
-	print("Running Record number",numChecked)
-	out=np.append(out, score(record))
-	numChecked+=1
-	if(numChecked%100 ==0):
-		print("#"*10,"\n")
-		print("Printed at",numChecked)
-		pd.Series(out).to_csv("../scoring2.csv",header=['score'],index=False)
-pd.Series(out).to_csv("../scores.csv",header=['score'],index=False)
+try:
+	with open("checkpoint",'r') as f:
+		checkpoint=int(f.read().strip())
+except FileNotFoundError:
+	checkpoint=-1
+	print("No Checkpoint Found")
+placeholder=pd.read_csv("randomFeatureGroupings.csv")
+df={'index':[],'score':[]}
+for index,record in placeholder.features.iteritems():
+	if(index>checkpoint):
+		print("Running Record number",index)
+		df['index'].append(index)
+		df['score'].append(score(record))
+		print
+		if(index%10==0):
+			print("#"*50)
+			print("Printed at",index)
+			df={'index':[],'score':[]}
+			with open("checkpoint",'w') as f:
+				f.write(str(index))
+
+
+pd.DataFrame(data=df).to_csv("intermediateResults.csv",mode="a",header=False,index=False)
+pd.DataFrame(data=df).to_csv("Final.csv",header=['index','score'],index=False)
